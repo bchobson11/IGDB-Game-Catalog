c1af6e4c718584aa4ac90f885d2506cd75895b01
Brandon Hobson <brandonhobson22@gmail.com>
2023-07-11 12:35:41 -0500

    combined all fetch calls with expand functionality from igdb api

--- client/src/App.js
+++ client/src/App.js
@@ -5,47 +5,29 @@ function App() {
   const [games, setGames] = useState([{}])
 
   useEffect(() => {
-    fetch("/api/games")
+    fetch("/api/games/")
       .then(response => response.json())
       .then(data => {setGames(data)})
     }, [])
 
-    
-
-    // {
-    //   "id": 1,
-    //   "cover": {
-    //     "id": 291911,
-    //     "image_id": "co698n"
-    //   },
-    //   "genres": [
-    //     "Shooter",
-    //     "Simulator",
-    //     "Adventure"
-    //   ],
-    //   "name": "Thief II: The Metal Age",
-    //   "rating": 86.68295195538036,
-    //   "rating_count": 118
-    // }
+  const image_url = 'https://images.igdb.com/igdb/image/upload/t_cover_small/'
 
   return (
     <>
       <h1>Games</h1>
 
       {games.length > 0? (
-        games.map(game => {
-          return (
-            <div key={game.id}>
-              {game.cover !== undefined? (
-                <img src={`https://images.igdb.com/igdb/image/upload/t_cover_small/${game.cover.image_id}.jpg`} alt={game.name}/>
-              ) : (
-                <img href='../public/blank.jpg' alt='blank img' />
-              )}
-              <h3>{game.name}</h3>
-              <p>{parseFloat(game.rating).toFixed(2)} | {game.rating_count}</p>
-            </div>
-          )
-        })
+        games.map(game => 
+          <div key={game.id}>
+            <h3>{game.name}</h3>
+            {game.cover !== undefined? (
+              <img src={`${image_url + game.cover.image_id}.jpg`} alt={game.name}/>
+            ) : (
+              <img href='../public/blank.png' alt='blank img' />
+            )}
+            <p>{parseFloat(game.rating).toFixed(2)} | {game.rating_count}</p>
+          </div>
+        )
       ) : (
         <p>Loading</p>
       )}
--- server/server.js
+++ server/server.js
@@ -8,46 +8,14 @@ const app = express()
 
     const gamesData = await fetchApi(
       'games', 
-      'fields name, cover, genres, rating, rating_count; sort id asc; limit 100;'
-    )
-    const coverData = await fetchApi(
-      'covers',
-      'fields image_id; sort game asc; limit 100;'
-    )
-    const genreData = await fetchApi(
-      'genres',
-      'fields *; limit 100;'
+      `
+        fields name, cover.image_id, genres.name, release_dates.platform.name, rating, rating_count; 
+        sort id asc; 
+        limit 100;
+      `
     )
     
-    // data manipulation
-    let data = gamesData;
-
-    data.map(game => {
-      game.genres = game.genres.map((g) => { return genreData.find((genre) => g === genre.id).name})
-      game.cover = {...coverData.find((cover) => (game.cover === cover.id))}
-    })
-
-    res.send(data)
+    res.json(gamesData)
   })
-
-  app.get("/api/covers", async (req, res) => {
-    res.json(
-      await fetchApi(
-      'covers', 
-      'fields game,image_id; limit 1; sort game asc;'
-      )
-    )
-  })
-
-  app.get("/api/genres", async (req, res) => {
-    res.json(
-      await fetchApi(
-      'genres', 
-      'fields name; limit 500; sort id asc;'
-      )
-    )
-  })
-
-
 
 app.listen(3500, () => {console.log("Server started on port 3500") })
