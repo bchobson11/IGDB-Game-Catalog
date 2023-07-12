4df317229e0397bc94817de6c24f368fff353b5d
Brandon Hobson <brandonhobson22@gmail.com>
2023-07-12 18:45:20 -0500

    Search function using API instead of RegEx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,42 +7,53 @@ export default function Home() {
 
   const [games, setGames] = useState([{}])
   const [search, setSearch] = useState("")
-
+  const [offset, setOffset] = useState(0) // default offset
+  const [limit, setLimit] = useState(100) // default number per page
+  
   useEffect(() => {
-    fetch("/api/games/")
+    fetch(`/api/games/`)
       .then(response => response.json())
       .then(data => {setGames(data)})
     }, [])
 
-  const handleChange = (e) => {
-    setSearch(e.target.value)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('submitted')
+    fetch(`/api/games/${search}`)
+      .then(response => response.json())
+      .then(data => {setGames(data)})
   }
 
   return (
     <>
       <h1>Games</h1>
 
-      <input 
-        placeholder='Search for Games'
-        value={search}
-        onChange={handleChange}
-      />
+      <form onSubmit={handleSubmit}>
+        <input 
+          placeholder='Search for Games'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
 
       {games.length > 0? (
         games.map(game => { 
-          let name = String(game.name).toLowerCase()
-          let regexp = new RegExp(search.toLowerCase())
-          if (regexp.test(name)) {
+          // let name = String(game.name).toLowerCase()
+          // let regexp = new RegExp(search.toLowerCase())
+          // if (regexp.test(name)) {
             return (
               <Link to={`game/${game.id}`}>
                 <GameCard {...game}/>
               </Link>  
             )
           } 
-        })
+        // }
+        )
       ) : (
         <p>No games found</p>
       )}
+
 
     </>
   )
--- server/server.js
+++ server/server.js
@@ -2,15 +2,32 @@ const fetchApi = require('./helper')
 const express = require('express')
 const app = express()
 
+  // 238,434 total games
+
   // GET ALL GAMES INFORMATION
   app.get("/api/games", async (req, res) => {
-
+    
     const gamesData = await fetchApi(
       'games', 
       `
         fields name, cover.image_id, genres.name, release_dates.platform.name, rating, rating_count; 
-        sort id asc; 
+        offset 0;
         limit 100;
+      `
+    )
+  
+    res.json(gamesData)
+  })
+
+  // Get games based on search
+  app.get("/api/games/:search", async (req, res) => {
+    const {search} = req.params
+    const gamesData = await fetchApi(
+      'games', 
+      `
+        fields name, cover.image_id, genres.name, release_dates.platform.name, rating, rating_count; 
+        limit 300;
+        search "${search}";
       `
     )
     
